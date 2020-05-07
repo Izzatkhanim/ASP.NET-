e7d1f3f91dbd90db1680e7fd96d152dfab5d68ec
izzetxanim@mail.ru <izzetxanim@mail.ru>
2020-05-07 23:23:03 +0400

    Repository and models created. Base entity class added

---
+++ Repository/Models/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Repository.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        [MaxLength(50)]
+        public string CreatedBy { get; set; }
+        [MaxLength(50)]
+        public string ModifiedBy { get; set; }
+
+    }
+}
